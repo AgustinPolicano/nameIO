44fbffc1991276ec69df26c19cd97a6bc18be409
Agustin Policano <apqubikdigital@hotmail.com>
2022-10-13 13:50:31 -0300

    feat: add styles for the home and edit the input next ui

---
+++ src/components/home/home.js
@@ -0,0 +1,12 @@
+import "./home.scss";
+
+function Home() {
+  return (
+    <div class="container">
+      <div class="typed-out">Name <span className="text-title">IO</span></div>
+      <p className="subtitle-text">Test your name!</p>
+    </div>
+  );
+}
+
+export default Home;
